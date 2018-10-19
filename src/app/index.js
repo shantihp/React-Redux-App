e615c00a9637439a84d64588ffcb8c1f9e1d6ada
Shantinath Chaudhary <shantissst@gmail.com>
2018-10-20 05:17:42 +0530

    Initial Redux code to demonstrate simple usage of Redux

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import {render} from "react-dom";
 
 import {User} from "./components/User";
@@ -27,4 +27,42 @@ class App extends React.Component {
 		);
 	}
 }
-render(<App/> , window.document.getElementById('reactReduxApp'));
+render(<App/> , window.document.getElementById('reactReduxApp'));*/
+
+/*Store, State and Dispatching of Action.
+We first create a store, we then tell the store who is responsible for changing the state 
+(the reducer), and then we dispatch actions on the store.*/
+import {createStore} from "redux";
+
+const reducer = (state, action) => {
+	switch(action.type){
+		case "ADD" :
+			state = state+action.payload;			 
+			break;
+		case "SUBTRACT" : 
+			state = state-action.payload;
+			break;
+	}
+	return state;
+};
+const store = createStore(reducer,1); // takes 2 arguments- reducer and initial app state
+
+store.subscribe(() => {
+	console.log("Store updated!", store.getState());	
+});
+
+// dispatching action now. The dispatch function is taking an Action as argument
+store.dispatch({
+	type: "ADD",
+	payload: 100	
+});
+
+store.dispatch({
+	type: "ADD",
+	payload: 22	
+});
+
+store.dispatch({
+	type: "SUBTRACT",
+	payload: 80	
+});
